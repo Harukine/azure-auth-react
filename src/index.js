2cc212d80089ba67ae65054b16f09236c39ca39a
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-07-27 22:57:41 +0900

    added msalInstance

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const msalInstance = new PublicClientApplication(msalConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MsalProvider>
+    <MsalProvider instance={msalInstance}>
       <App />
     </MsalProvider>
   </React.StrictMode>
